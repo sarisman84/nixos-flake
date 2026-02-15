46df673fe95d6742a0cc5e775153472800546ed9
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-02-15 17:59:24 +0100

    Updated some settings for nv_plasma

--- hosts/nv_plasma/networking/default.nix
+++ hosts/nv_plasma/networking/default.nix
@@ -3,7 +3,7 @@
     networking.networkmanager.enable = true;
 
     networking.hostName = "spyro-nv_plasma"; # Define your hostname.
-    networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant. 
+    networking.wireless.enable = true ;  # Enables wireless support via wpa_supplicant. 
 
     # Configure network proxy if necessary
     #networking.proxy.default = "http://user:password@proxy:port/";
