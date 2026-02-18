4880319c70daaa66ef91c7dffba97499e4b0d4d4
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-02-18 02:32:23 +0100

    - Added Aliases to shorthand some commands - Updated VSCode to include a way to import its user settings from a   .json file. - Resolved some Nix LSP stuff with VSCode. - Added Kitty. - Updated the theme of all terminals to use Starship. - Enabled Bash.

--- users/spyro/modules/productivity/bash/default.nix
+++ users/spyro/modules/productivity/bash/default.nix
@@ -1,6 +1,20 @@
 {...}:
 {
-  programs.bash.enable = true;
+  programs.bash = {
+     enable = true;
+     shellAliases = {
+        dnCount = "ls **/default.nix | wc -l";
+        dnlCount = "cat **/default.nix | wc -l";
+
+        check = "git status";
+        commit = "git add . | git commit -m";
+        linkRepo = "git remote add origin";
+        push = "git push";
+        pull = "git pull --rebase";
+        switch = "git switch";
+        create = "git switch -c";
+     };
+  };
   programs.starship = {
      enable = true;
      enableBashIntegration = true;
