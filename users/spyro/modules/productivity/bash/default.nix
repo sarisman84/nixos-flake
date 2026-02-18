fdd93e99dfb27c87fb339fabefe3e534b22b18fc
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-02-18 01:11:59 +0100

    Added Starship Config.toml and imported it to bash's nix file

--- users/spyro/modules/productivity/bash/default.nix
+++ users/spyro/modules/productivity/bash/default.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{...}:
 {
   programs.bash.enable = true;
   programs.starship = {
      enable = true;
      enableBashIntegration = true;
+     settings = builtins.fromToml (builtins.readFile ./starship_config.toml);
   };
 }
