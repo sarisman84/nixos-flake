ef92c1394118fdc1ae38b8e7b3c97cb95489d72e
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-03-04 01:39:00 +0100

    Enabled Auto-Update and Auto-Garbage Collect

--- shared/modules/general.nix
+++ shared/modules/general.nix
@@ -1,6 +1,5 @@
-{
-  pkgs,
-  ...
+{ pkgs
+, ...
 }:
 {
   nix.settings.experimental-features = [
@@ -13,7 +12,7 @@
 
   # Fonts
   fonts.packages = with pkgs; [
-     nerd-fonts.fira-code
+    nerd-fonts.fira-code
   ];
 
   # List packages installed in system profile. To search, run:
@@ -34,4 +33,23 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
+
+  # Auto Update
+  system.autoUpgrade = {
+    enable = true;
+    flake = "../../flake.nix";
+    flags = [
+      "--print-build-logs"
+      "--commit-lock-file" # If you want to automatically commit the updated flake.lock
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+  # Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 300";
+  };
+  nix.settings.auto-optimise-store = true;
 }
