b5a6f89758beefa57590d902fd74d46cdc7b1cdf
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-04-07 00:04:44 +0200

    Wrapped several packages to jetbrain's rider for Unity as well as aspnet

--- users/spyro/modules/productivity/jetbrains/default.nix
+++ users/spyro/modules/productivity/jetbrains/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, flake-inputs, ... }:
+{ pkgs, lib, flake-inputs, ... }:
+let
+  ext = import ./patched_jetbrains_rider.nix { inherit pkgs; inherit lib; };
+in
 {
   imports = [
     flake-inputs.nix-flatpak.homeManagerModules.nix-flatpak
@@ -7,6 +10,7 @@
   home = {
     packages = with pkgs;[
       jetbrains-toolbox
+      ext.rider
     ];
   };
 }
---
+++ users/spyro/modules/productivity/jetbrains/patched_jetbrains_rider.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, ... }:
+let
+  extra-path = with pkgs; [
+    dotnet-aspnetcore_10
+    dotnet-sdk_10
+    dotnetPackages.Nuget
+    mono
+    msbuild
+  ];
+
+  extra-lib = with pkgs; [
+
+  ];
+in
+{
+  rider = pkgs.jetbrains.rider.overrideAttrs (attrs: {
+    postInstall = ''
+      # Wrap rider with extra tools and libraries
+      mv $out/bin/rider $out/bin/.rider-toolless
+      makeWrapper $out/bin/.rider-toolless $out/bin/rider \
+        --argv0 rider \
+        --prefix PATH : "${lib.makeBinPath extra-path}" \
+        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath extra-lib}"
+
+      # Making Unity Rider plugin work!
+      # The plugin expects the binary to be at /rider/bin/rider,
+      # with bundled files at /rider/
+      # It does this by going up two directories from the binary path
+      # Our rider binary is at $out/bin/rider, so we need to link $out/rider/ to $out/
+      shopt -s extglob
+      ln -s $out/rider/!(bin) $out/
+      shopt -u extglob
+    '' + attrs.postInstall or "";
+  });
+}
