46fcf4723abfec00dda6b7104b136179cdd81ffd
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-05-01 14:22:08 +0200

    trying something else

--- users/spyro/modules/productivity/ssh/default.nix
+++ users/spyro/modules/productivity/ssh/default.nix
@@ -1,16 +1,23 @@
-{ ... }:
+{ lib, ... }:
 {
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      openssh = prev.openssh.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [ ./openssh.patch ];
-        doCheck = false;
-      });
-    })
-  ];
-
   programs.ssh = {
     enable = true;
+  };
+
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/322
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
   };
 }
