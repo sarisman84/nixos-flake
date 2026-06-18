399f7d05e40d76ba4617393033175e8eb359c9e9
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-06-18 12:27:54 +0200

    Updated rsi-launcher.nix

--- hosts/nv_plasma/modules/entertainment/rsi-launcher.nix
+++ hosts/nv_plasma/modules/entertainment/rsi-launcher.nix
@@ -1,29 +1,35 @@
-{pkgs, config, lib, inputs, ...}:
+{ pkgs, inputs, ... }:
 {
-   imports = [
-     inputs.nix-citizen.nixosModules.default
-   ];
+  imports = [
+    inputs.nix-citizen.nixosModules.default
+  ];
 
-   nix.settings = {
-      substituters = [
-        "https://nix-gaming.cachix.org"
-        "https://nix-citizen.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
-      ];
-   };
+  nix.settings = {
+    substituters = [
+      "https://nix-gaming.cachix.org"
+      "https://nix-citizen.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+    ];
+  };
 
-   programs.rsi-launcher = {
-      enable = true;
-      preCommands = ''
-        export DXVK_NVAPI_SET_NGX_DEBUG_OPTIONS="DLSSIndicator=1,DLSSGIndicator=1"
-        export VK_LOADER_LAYERS_ENABLE=VK_LAYER_MESA_vram_report_limit
-        export VK_VRAM_REPORT_LIMIT_HEAP_SIZE=9216
-        export VK_VRAM_REPORT_LIMIT_DEVICE_ID=0x10de:0x2b85
-      '';
-   };
+  programs.rsi-launcher = {
+    enable = true;
+    preCommands = ''
+              export DXVK_NVAPI_SET_NGX_DEBUG_OPTIONS="DLSSIndicator=1,DLSSGIndicator=1"
+              export VK_LOADER_LAYERS_ENABLE=VK_LAYER_MESA_vram_report_limit
+              export VK_VRAM_REPORT_LIMIT_HEAP_SIZE=29696
+              export VK_VRAM_REPORT_LIMIT_DEVICE_ID=0x10de:0x2b85
+
+              export LANG=se_SE
+
+              export XMODIFIERS="@im=none"
+              export GTK_IM_MODULE="xim"
+              export QT_IM_MODULE="xim"
+    ''; 
+  };
 
   # NixOS configuration for Star Citizen requirements
   boot.kernel.sysctl = {
